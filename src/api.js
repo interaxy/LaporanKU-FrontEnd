f3d2fdffc02c8ab90cca0bc0d22cf01ce73d33fb
Njani <akhmdkhrlyn.343@gmail.com>
2025-11-22 10:43:45 +0700

    api.js

--- src/api.js
+++ src/api.js
@@ -10,27 +10,50 @@
 //   return config
 // })
 
+// import axios from 'axios';
+
+// export const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// });
+
+// // selipkan token setiap request
+// API.interceptors.request.use((config) => {
+//   const t = localStorage.getItem('token');
+//   if (t) config.headers.Authorization = `Bearer ${t}`;
+//   return config;
+// });
+
+// // (opsional) auto-logout saat 401
+// API.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err?.response?.status === 401) {
+//       localStorage.removeItem('token');
+//       localStorage.removeItem('user');
+//       // window.location.href = '/login'; // aktifkan kalau mau langsung redirect
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://laporanku-backend.onrender.com/api',
 });
 
-// selipkan token setiap request
 API.interceptors.request.use((config) => {
   const t = localStorage.getItem('token');
   if (t) config.headers.Authorization = `Bearer ${t}`;
   return config;
 });
 
-// (opsional) auto-logout saat 401
 API.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err?.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      // window.location.href = '/login'; // aktifkan kalau mau langsung redirect
     }
     return Promise.reject(err);
   }
