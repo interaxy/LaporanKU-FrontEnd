517ccbf265daadfa99c97128bfeb85ecef4cfe4d
Njani <152503125+akhmdkhrlyn@users.noreply.github.com>
2025-11-22 16:06:12 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -38,8 +38,11 @@
 
 import axios from 'axios';
 
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+
 export const API = axios.create({
-  baseURL: 'https://laporanku-backend.onrender.com/api',
+  baseURL: BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
